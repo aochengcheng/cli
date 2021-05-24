d62f614863e22e2a17caee5458bf1faca8f4af0b
togetherEnjoy <1156438031@qq.com>
2021-05-24 18:07:19 +0800

    feat: 创建软连接、本地调试脚手架

---
+++ cli-ac/bin/index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+// /Users/chengzi/.nvm/versions/node/v14.15.4/lib/node_modules
+// /Users/chengzi/.nvm/versions/node/v14.15.4/bin
+
+const lib = require('cli-lib-ac');
+
+console.log(lib);
+
+console.log('cli-ac12121212');
